9380089a22c9606d63d94b9ff634931557f5e152
Matthieu <matthieu@pixel-developpement.com>
2023-10-02 15:55:19 +0200

    + add github actions

---
+++ ecs.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/index.php',
+    ]);
+
+    $ecsConfig->skip([
+        __DIR__ . '/kirby',
+    ]);
+
+    // this way you add a single rule
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    // this way you can add sets - group of rules
+    $ecsConfig->sets([
+        // run and fix, one by one
+        // SetList::SPACES,
+        // SetList::ARRAY,
+        // SetList::DOCBLOCK,
+        // SetList::NAMESPACES,
+        // SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
+};
