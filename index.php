0e0daebfbee7bb9c91135836a157acf8a756a6ca
Matthieu <matthieu@pixel-developpement.com>
2025-05-20 16:10:10 +0200

    + Refactoring code

--- index.php
+++ index.php
@@ -6,18 +6,26 @@ Kirby::plugin('pixelopen/asset-version', [
     ],
     'components' => [
         'css' => function ($kirby, $url) {
-            if ($kirby->option('pixelopen.asset-version.active')) {
-                return dirname($url) . '/' . F::name($url) . '.css?v=' . F::modified(Url::path($url));
-            } else {
-                return $url;
-            }
+            return addVersionToAsset($kirby, $url, 'css');
         },
         'js' => function ($kirby, $url) {
-            if ($kirby->option('pixelopen.asset-version.active')) {
-                return dirname($url) . '/' . F::name($url) . '.js?v=' . F::modified(Url::path($url));
-            } else {
-                return $url;
-            }
+            return addVersionToAsset($kirby, $url, 'js');
         }
     ]
 ]);
+
+/**
+ * Adds a version parameter to an asset file based on its modification date
+ *
+ * @param \Kirby\Cms\App $kirby Kirby instance
+ * @param string $url Asset URL
+ * @param string $extension File extension (css, js, etc.)
+ * @return string URL with added version or original URL
+ */
+function addVersionToAsset($kirby, $url, $extension) {
+    if ($kirby->option('pixelopen.asset-version.active')) {
+        return dirname($url) . '/' . F::name($url) . '.' . $extension . '?v=' . F::modified(Url::path($url));
+    } else {
+        return $url;
+    }
+}
